57a4d661b007e9c1000e43df17af748f68bfb308
hanwantparihar <hanwantsingh.anques@gmail.com>
2026-06-18 14:57:25 +0530

    Fix Vercel config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // reactStrictMode: true,
-//   // output: 'export',
-//   distDir: 'build',
-//   images: {
-//     unoptimized: true
-//   }
-// }
-
-// module.exports = nextConfig
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: 'build',
-}
+const nextConfig = {}
 
 module.exports = nextConfig
